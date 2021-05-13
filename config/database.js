15334664906018d77c60aab80db7952d2ca895d7
tijariomilitar <tijariomilitar@gmail.com>
2021-05-12 22:02:12 -0300

    fix

--- config/database.js
+++ config/database.js
@@ -14,7 +14,7 @@ module.exports = {
 	    }
 	},
 	production: {
-	    url: 'http://jariomilitar.com',
+	    url: 'http://www.jariomilitar.com',
 	    database: {
 		    host: process.env.DB_PRODUCTION_HOST,
 	        port: process.env.DB_PRODUCTION_PORT,
