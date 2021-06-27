bf639ae3bcb371ff13451c6cd6d6c7d1a11a939a
tijariomilitar <tijariomilitar@gmail.com>
2021-06-27 20:54:31 -0300

    redirect to https

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
-const lib = require('../../config/lib');
 
-// function redirectToHttps(req, res, next) => {
-// 	if ((req.headers["x-forwarded-proto"] || "").endsWith("http")){
-// 		res.redirect(`https://${req.headers.host}${req.url}`); 
-// 	} else {
-//     	next();
-// 	}
-// };
+function redirectToHttps(req, res, next) {
+	console.log(req.headers["x-forwarded-proto"]);
+	if ((req.headers["x-forwarded-proto"] || "").endsWith("http") || req.headers["x-forwarded-proto"] == undefined || req.headers["x-forwarded-proto"] == null || req.headers["x-forwarded-proto"] == ""){
+		res.redirect(`https://${req.headers.host}${req.url}`); 
+	} else {
+    	next();
+	}
+};
 
 const homeController = require("../controller/home");
 
-router.get("/", homeController.index);
+router.get("/", redirectToHttps, homeController.index);
 
 router.get("/login", homeController.login);
 router.get("/signup", homeController.signup);
