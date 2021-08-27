d77277bee3399249989a9ccb313017a4b63895a9
tijariomilitar <tijariomilitar@gmail.com>
2021-08-27 09:37:12 -0300

    route to https catalogs

--- app/routes/catalog.js
+++ app/routes/catalog.js
@@ -3,11 +3,11 @@ const lib = require("jarmlib");
 
 const catalogController = require('../controller/catalog');
 
-router.get("/", catalogController.index);
-router.get("/varejo", catalogController.retail);
-router.get("/atacado", catalogController.wholesale);
-router.get("/representantes", catalogController.agent);
-router.get("/upsell", catalogController.upsell);
+router.get("/", lib.route.toHttps, catalogController.index);
+router.get("/varejo", lib.route.toHttps, catalogController.retail);
+router.get("/atacado", lib.route.toHttps, catalogController.wholesale);
+router.get("/representantes", lib.route.toHttps, catalogController.agent);
+router.get("/upsell", lib.route.toHttps, catalogController.upsell);
 
 router.post("/filter", catalogController.filter);
 router.post('/product/find', catalogController.product.find);
