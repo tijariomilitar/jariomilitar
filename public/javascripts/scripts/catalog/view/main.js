6e6528e56ae6d849b2ff0ac8ac7041c04a2765a8
tijariomilitar <tijariomilitar@gmail.com>
2021-10-29 10:06:50 -0300

    ultimas alteracoes na interface blackout

--- public/javascripts/scripts/catalog/view/main.js
+++ public/javascripts/scripts/catalog/view/main.js
@@ -25,3 +25,10 @@ Catalog.view.filter = (products, pagination) => {
 		document.getElementById("catalog-filter-box").style.display = "";
 	};
 }
+
+html += "<div class='box b5 container ground margin-top-10 padding-10 pointer shadow-2-hover' onclick='Catalog.product.controller.show(`"+products[i].id+"`, `"+products[i].category_id+"`)'>";
+	html += "<div class='box b1'><img class='image-card' src='"+products[i].image+"' alt=''/></div>";
+	html += "<div class='box b1'><img class='image-card' src='/images/product/no-product.png' alt=''/></div>";
+html += `<div class="box b1 avant-garde center padding-10 bold" style="color:#323232;">`+products[i].name+` - `+products[i].color+`</div>`;
+if(products[i].price){ html += "<div class='box b1 em15 center italic bold' style='color:#467846;'>$"+products[i].price.toFixed(2)+"</div>" }
+html += "</div>";
