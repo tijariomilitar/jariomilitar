92acc1f699074c0a3843304216a510ba3837d729
hlyras <hlyras2011@gmail.com>
2021-11-05 14:44:00 -0300

    change $ to R$

--- public/javascripts/scripts/catalog/view/main.js
+++ public/javascripts/scripts/catalog/view/main.js
@@ -15,7 +15,7 @@ Catalog.view.filter = (products, pagination) => {
 				html += "<div class='box b1'><img class='image-card' src='/images/product/no-product.png' alt=''/></div>";
 			};
 			html += `<div class="box b1 avant-garde center padding-10 bold" style="color:#323232;">`+products[i].name+` - `+products[i].color+`</div>`;
-			if(products[i].price){ html += "<div class='box b1 em15 center italic bold' style='color:#467846;'>$"+products[i].price.toFixed(2)+"</div>" }
+			if(products[i].price){ html += "<div class='box b1 em15 center italic bold' style='color:#467846;'>R$"+products[i].price.toFixed(2)+"</div>" }
 			html += "</div>";
 		};
 		document.getElementById("catalog-filter-div").innerHTML = html;
@@ -30,5 +30,5 @@ html += "<div class='box b5 container ground margin-top-10 padding-10 pointer sh
 	html += "<div class='box b1'><img class='image-card' src='"+products[i].image+"' alt=''/></div>";
 	html += "<div class='box b1'><img class='image-card' src='/images/product/no-product.png' alt=''/></div>";
 html += `<div class="box b1 avant-garde center padding-10 bold" style="color:#323232;">`+products[i].name+` - `+products[i].color+`</div>`;
-if(products[i].price){ html += "<div class='box b1 em15 center italic bold' style='color:#467846;'>$"+products[i].price.toFixed(2)+"</div>" }
+if(products[i].price){ html += "<div class='box b1 em15 center italic bold' style='color:#467846;'>R$"+products[i].price.toFixed(2)+"</div>" }
 html += "</div>";
