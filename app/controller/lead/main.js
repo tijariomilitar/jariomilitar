630d029a7d6e8114b0a6c4243855b5eee1511120
tijariomilitar <tijariomilitar@gmail.com>
2022-12-01 10:25:47 -0300

    update framework from heroku to digital ocean

--- app/controller/lead/main.js
+++ app/controller/lead/main.js
@@ -15,12 +15,12 @@ leadController.save = async (req, res) => {
 	lead.email = req.body.email;
 	lead.phone = req.body.phone;
 
-	if(!lead.datetime) { return res.send({ msg: "Ocorreu um erro ao realizar seu cadastro por favor atualize a página." }); }
-	if(!lead.name) { return res.send({ msg: "Por favor preencha seu nome para que possamos te conhecer." }); }
-	if(!lead.email) { return res.send({ msg: "É necessário preencher seu email para que possamos lhe enviar nosso catálogo" }); }
-	if(!lead.phone) { return res.send({ msg: "Por favor preencha seu telefone para que possamos entrar em contato." }); }
+	if (!lead.datetime) { return res.send({ msg: "Ocorreu um erro ao realizar seu cadastro por favor atualize a página." }); }
+	if (!lead.name) { return res.send({ msg: "Por favor preencha seu nome para que possamos te conhecer." }); }
+	if (!lead.email) { return res.send({ msg: "É necessário preencher seu email para que possamos lhe enviar nosso catálogo" }); }
+	if (!lead.phone) { return res.send({ msg: "Por favor preencha seu telefone para que possamos entrar em contato." }); }
 
-	try	{
+	try {
 		await lead.save();
 		res.send({ done: "Muito obrigado, em breve um de nossos consultores entrará em contato." });
 	} catch (err) {
